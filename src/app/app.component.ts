8b83df15b6c729497f035e8ec976d47710697157
benyehuda.yael2 <benyehuda.yael2@gmail.com>
2022-12-13 21:24:37 +0200

    yael technique

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-Terminal-Dashboard';
+  menuIconClicked = false;
+  constructor(){
+  }
+  openMenu(){
+    this.menuIconClicked = !this.menuIconClicked;
+  }
 }
